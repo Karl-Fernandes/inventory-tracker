f9459731c6a994b15328b4bbe31e2e799be170f8
KARL <krf0821@gmail.com>
2024-08-06 20:15:56 +0100

    Added working pages

--- app/components/Header.js
+++ app/components/Header.js
@@ -11,15 +11,25 @@ const Header = () => {
           <nav className="bg-blue-700 py-4 px-6 flex items-center
           justify-between mb-5">
             <div className="flex items-center">
-              <Link href="/">
+            {userId && (
+              <Link href="/dashboard">
                 <div className="text-lg uppercase font-bold text-white">
                   Pantry App
                 </div>
               </Link>
+               )}
+            {!userId && (
+          <h1>
+          <div className="text-lg uppercase font-bold text-white">
+            Pantry App
+          </div>
+        </h1>
+        )}
             </div>
             <div className="text-white flex items-center">
               {!userId && (
                 <>
+                
               <Link href="sign-in" className="text-gray-300 hover:text-white
               mr-4">Sign In</Link>
               <Link href="sign-up" className="text-gray-300 hover:text-white
