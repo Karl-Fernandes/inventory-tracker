18a8d85e1f95e15280f6bcbb335b5f8061fc5e1f
KARL <krf0821@gmail.com>
2024-08-06 18:30:17 +0100

    Added sign in page

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import Header from "./components/Header";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,9 +13,28 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  
+
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider 
+    appearance={{
+      baseTheme: dark,
+    }}>
+      
+      <html lang="en">
+        <body className={inter.className}>
+          <Header />
+          
+          <main className="container mx-auto">
+            <div className="flex items-start justify-center min-h-screen">
+              <div className='mt-20'>
+              {children}
+              </div>
+            </div>
+          </main>
+
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- app/page.js
+++ app/page.js
@@ -64,58 +64,58 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
-        <h1 className="text-4xl p-4 text-center">Pantry Tracker</h1>
-        <div className="bg-slate-800 p-4 rounded-lg">
-          <form className="grid grid-cols-6 gap-3 items-center text-black mb-4" onSubmit={addItem}>
-            <input 
-              value={newItem.name}
-              onChange={(e) => setNewItem({ ...newItem, name: e.target.value })}
-              className="col-span-3 p-3 border rounded" 
-              type="text" 
-              placeholder="Enter Item" 
-            />
-            <input 
-              value={newItem.amount}
-              onChange={(e) => setNewItem({ ...newItem, amount: e.target.value })}
-              className="col-span-2 p-3 border rounded" 
-              type="number" 
-              placeholder="Enter Quantity" 
-            />
-            <button 
-              className="text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl rounded" 
-              type="submit"
-            >
-              <FontAwesomeIcon icon={faPlus} />
-            </button>
-            <input 
-              onChange={(e) => setSearchQuery(e.target.value)}
-              className="col-span-6 p-3 border rounded" 
-              type="text" 
-              placeholder="Search..." 
-            />
-          </form>
-          <ul>
-            {filteredItems.map((item, id) => (
-              <li 
-                key={id} 
-                className='my-4 w-full flex justify-between items-center bg-slate-950 rounded'
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+          <h1 className="text-4xl p-4 text-center">Pantry Tracker</h1>
+          <div className="bg-slate-800 p-4 rounded-lg">
+            <form className="grid grid-cols-6 gap-3 items-center text-black mb-4" onSubmit={addItem}>
+              <input 
+                value={newItem.name}
+                onChange={(e) => setNewItem({ ...newItem, name: e.target.value })}
+                className="col-span-3 p-3 border rounded" 
+                type="text" 
+                placeholder="Enter Item" 
+              />
+              <input 
+                value={newItem.amount}
+                onChange={(e) => setNewItem({ ...newItem, amount: e.target.value })}
+                className="col-span-2 p-3 border rounded" 
+                type="number" 
+                placeholder="Enter Quantity" 
+              />
+              <button 
+                className="text-white bg-slate-950 hover:bg-blue-700 p-3 text-xl rounded transition duration-300 ease-in-out transform hover:scale-105"
+                type="submit"
               >
-                <div className='p-4 w-full flex justify-between items-center'>
-                  <span className='capitalize flex-grow'>{item.name}</span>
-                  <span className='amount ml-3'>x{item.amount}</span>
-                </div>
-                <button 
-                  onClick={() => deleteItem(item.id)}
-                  className='ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16 rounded-r'
+                <FontAwesomeIcon icon={faPlus} />
+              </button>
+              <input 
+                onChange={(e) => setSearchQuery(e.target.value)}
+                className="col-span-6 p-3 border rounded" 
+                type="text" 
+                placeholder="Search..." 
+              />
+            </form>
+            <ul>
+              {filteredItems.map((item, id) => (
+                <li 
+                  key={id} 
+                  className='my-4 w-full flex justify-between items-center bg-slate-950 rounded'
                 >
-                  <FontAwesomeIcon icon={faTrash} />
-                </button>
-              </li>
-            ))}
-          </ul>
+                  <div className='p-4 w-full flex justify-between items-center'>
+                    <span className='capitalize flex-grow'>{item.name}</span>
+                    <span className='amount ml-3'>x{item.amount}</span>
+                  </div>
+                  <button 
+                    onClick={() => deleteItem(item.id)}
+                    className='ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16 rounded-r'
+                  >
+                    <FontAwesomeIcon icon={faTrash} />
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
     </main>
   );
 }
